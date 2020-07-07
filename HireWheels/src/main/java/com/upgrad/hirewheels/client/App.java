d0d22598ec6c3fa3168b4f1b625ad63058bd90b0
kavitshah14 <66173079+kavitshah14@users.noreply.github.com>
2020-07-07 23:09:45 +0530

    Update App.java

--- HireWheels/src/main/java/com/upgrad/hirewheels/client/App.java
+++ HireWheels/src/main/java/com/upgrad/hirewheels/client/App.java
@@ -18,9 +18,6 @@ public class App {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        /*===================================================================================================================================*/
-
-        System.out.println("-------------------------------------Checkpoint 1 Solution---------------------------------------");
 
         User user = (User) context.getBean("user1");
         System.out.println(user);
@@ -28,50 +25,40 @@ public class App {
         BalanceService balanceService = (BalanceService) context.getBean("balanceService");
         System.out.println(balanceService.checkUserBalance());
 
-        System.out.println("-------------------------------------Checkpoint 2 Solution---------------------------------------");
+
 
         UserService userService = (UserService) context.getBean("userService");
         System.out.println(userService);
 
-        /*===================================================================================================================================*/
-
-        System.out.println("-------------------------------------Checkpoint 3 Solution---------------------------------------");
 
         UserDAO userDAO = (UserDAO) context.getBean("userDAO");
 
-        Users users = new Users("Ram", "Kumar", "password", "ramkumuar@4.com", "9795958876", 10000);
+        Users users = new Users("Kavit", "Shah", "12345678", "kavitshah14.com", "979598476", 20000);
 
-
-        System.out.println("-------------------------------------Inserted Users Details---------------------------------------");
         Users savedUser = userDAO.insertUsersDetails(users);
 
-        System.out.println("-------------------------------------Fetch All User Details---------------------------------------");
         System.out.println(userDAO.fetchAllUsersDetails());
 
-        System.out.println("-------------------------------------Fetch User Details by Id---------------------------------------");
+
         System.out.println(userDAO.fetchUserDetails(2));
 
-        /*System.out.println("-------------------------------------Deleted User Detail by Id---------------------------------------");
         System.out.println(userDAO.deleteUser(2));*/
 
-        System.out.println("-------------------------------------Inserted Activity---------------------------------------");
+  
         ActivityDAO activityDAO = (ActivityDAO) context.getBean("activityDAO");
         Activity activity = new Activity("A");
 
         Activity savedActivity = activityDAO.save(activity);
         System.out.println(savedActivity);
-        System.out.println("-------------------------------------Fetch All Activity---------------------------------------");
         System.out.println(activityDAO.findAll());
 
-        System.out.println("------------------------------------ Fetch Activity By Id---------------------------------------");
         System.out.println(activityDAO.findById(2));
 
-        System.out.println("-------------------------------------Updated Activity---------------------------------------");
         Activity activity1 = activityDAO.findById(activity.getActivityId()).get();
         activity1.setActivityType("B");
         System.out.println(activityDAO.save(activity1));
 
-        System.out.println("-------------------------------------Delete Activity By Id---------------------------------------");
+
         activityDAO.deleteById(2);
 
 
